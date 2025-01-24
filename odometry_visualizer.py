f79d955caa19eafbbf12b9cc0c1ed4113878d6c0
TheIllusionist77 <113849310+TheIllusionist77@users.noreply.github.com>
2025-01-23 20:31:19 -0500

    Update odometry_visualizer.py

--- odometry_visualizer.py
+++ odometry_visualizer.py
@@ -1,10 +1,10 @@
-import pygame, os, math, random, time
+import pygame, os, random, ast
 
 pygame.init()
 
 WIDTH = 1280
 HEIGHT = 720
-SCALE = 8
+SCALE = 10
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 log_directory = "/Users/sanjitprakash/Downloads"
@@ -22,84 +22,50 @@ def read_file(directory):
 
 def parse_data(log_data):
     path = []
-    objects_locations = {}
-    last_disp = 0
 
     for line in log_data:
         parts = line.strip().split(", ")
-        
-        disp = -float(parts[0][1:]) + float(parts[1]) + float(parts[2]) - last_disp
-        last_disp = disp + last_disp
+        x = parts[0][1:]
+        y = parts[1]
 
-        yaw = float(parts[3])
-        tof_reading = float(parts[4])
-        if parts[5][:-1] != "None":
-            object = parts[5][:-1]
-        else:
-            object = None
+        if line[:1] == "[":
+            break
 
-        path.append((disp, tof_reading, yaw, object))
-        
-        if object != None and object not in objects_locations:
-            objects_locations[object] = []
-            objects_locations[object].append([disp, yaw, tof_reading])
-        elif object in objects_locations:
-            objects_locations[object].append([disp, yaw, tof_reading])
+        path.append((x, y, parts[2][:-1]))
 
-    objects = {}
-    for object in objects_locations:
-        object_x, object_y = 0, 0
-        count = 0
+    object_locations = ast.literal_eval(line)
+    print(object_locations)
+    return path, object_locations
 
-        for each in objects_locations[object]:
-            count += 1
-            disp, tof, yaw = each
-
-            yaw *= -1
-
-            if count == 1:
-                initial_angle = yaw - 90
-
-            yaw -= initial_angle
-
-            object_x += (disp + int(-tof / 3.4 * math.cos(yaw))) * SCALE
-            object_y += (disp + int(-tof / 3.4 * math.sin(yaw))) * SCALE
-
-        object_x /= count
-        object_y /= count
-        objects[object] = object_x, object_y
-    
-    return path, objects
-
-def draw_path(path, objects):
-    SCREEN.fill((255, 255, 255))
-    x, y = WIDTH / 2, HEIGHT / 2
+def draw_path(path, object_locations):
+    SCREEN.fill((0, 0, 0))
+    center_x, center_y = WIDTH / 2, HEIGHT / 2
     count = 0
 
     for point in path:
         count += 1
-        disp, tof, yaw, obj = point
-        yaw *= -1
+        disp_x, disp_y, name = point
 
-        if count == 1:
-            initial_angle = yaw - 90
+        x = (float(disp_x) * SCALE) + center_x
+        y = (float(disp_y) * SCALE) + center_y
 
-        yaw -= initial_angle
-        
-        x += float(disp * math.cos(math.radians(yaw))) * SCALE
-        y += float(disp * math.sin(math.radians(yaw))) * SCALE
+        print(x, y)
 
         pygame.draw.circle(SCREEN, (0, min(255, count), 255), (x, y), 1)
         pygame.display.update()
 
-    for object in objects:
-        object_x, object_y = objects[object]
+    for object in object_locations:
+        name, x, y, last_seen = object
+
+        x = (float(disp_x) * SCALE) + center_x
+        y = (float(disp_y) * SCALE) + center_y
+
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        pygame.draw.circle(SCREEN, color, (WIDTH / 2 + object_x, HEIGHT / 2 + object_y), 5)
+        pygame.draw.circle(SCREEN, color, (x, y), 5)
 
         font = pygame.font.SysFont("Arial", 12)
-        text = font.render(object, True, color)
-        SCREEN.blit(text, (WIDTH / 2 + object_x + 10, HEIGHT / 2 + object_y - 15))
+        text = font.render(name, True, color)
+        SCREEN.blit(text, (x + 10, y - 15))
         pygame.display.update()
 
 log_data = read_file(log_directory)
@@ -108,7 +74,7 @@ if log_data is None:
     print("No log files found.")
     running = False
 else:
-    path, objects = parse_data(log_data)
+    path, object_locations = parse_data(log_data)
     running = True
 
 drawn = False
@@ -118,7 +84,7 @@ while running:
             running = False
 
     if not drawn:
-        draw_path(path, objects)
+        draw_path(path, object_locations)
         drawn = True
 
 pygame.quit()
